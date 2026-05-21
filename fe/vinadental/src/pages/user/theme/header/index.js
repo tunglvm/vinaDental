2806626a1e9f64d4f531fb85c7b35e87caa6b0a3
tunglvm <dotunglam2k5@gmail.com>
2026-05-21 19:24:08 +0700

    fixing sass bugs

--- fe/vinadental/src/pages/user/theme/header/index.js
+++ fe/vinadental/src/pages/user/theme/header/index.js
@@ -1,6 +1,9 @@
 import { memo } from "react";
+import { Link } from "react-router-dom";
 import "./style.scss";
 import { FaFacebook } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
+import { FaUser } from "react-icons/fa6";
 
 const Header = () => {
     return (
@@ -10,9 +13,24 @@ const Header = () => {
                     <div className="col-6"></div>
                     <div className="col-6 header__top_right">
                         <ul>
-                            <li><FaFacebook /></li>
-                            <li></li>
-                            <li></li>
+                            <li>
+                                <Link to = {""}>
+                                    <FaFacebook />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to = {""}>
+                                    <FaInstagram />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to = {""}>
+                                    <FaUser />
+                                </Link>
+                                <Link to = {""}>
+                                    <span>Đăng nhập</span>
+                                </Link>
+                            </li>
                         </ul>
                     </div>
                 </div>
